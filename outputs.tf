97eecc78d504376c3a79d1bdca983186b7a4a009
Ravi <ravi@gmail.com>
2022-08-07 09:41:52 +0530

    ec2 code

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "PUBLIC_LB_ARN" {
 output "PRIVATE_LB_ARN" {
   value = aws_lb.alb-private.arn
 }
+
+output "PRIVATE_LB_DNS" {
+  value = aws_lb.alb-private.dns_name
+}
+
+output "PUBLIC_LB_DNS" {
+  value = aws_lb.alb-public.dns_name
+}

59adf560612cdb20c1de1a10944bccf3878f7901
Ravi <ravi@gmail.com>
2022-08-07 10:25:08 +0530

    ec2 code

--- private-lb.tf
+++ private-lb.tf
@@ -14,8 +14,6 @@ resource "aws_lb_listener" "backend" {
   load_balancer_arn = aws_lb.alb-private.arn
   port              = 80
   protocol          = "HTTP"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
   default_action {
     type = "fixed-response"

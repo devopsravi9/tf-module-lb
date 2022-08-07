f9232449502b1a1f646380f10f3a88c7963dc39a
Ravi <ravi@gmail.com>
2022-08-07 09:35:51 +0530

    ec2 code

---
+++ route-53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "public" {
+  zone_id = var.PUBLIC_ZONE_ID
+  name    = "app-devopsravi.online"
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_lb.alb-public.dns_name]
+}
--- vars.tf
+++ vars.tf
@@ -4,3 +4,4 @@ variable "ALLOW_SG_CIDR" {}
 variable "VPC_ID" {}
 variable "PRIVATE_SUBNET_ID" {}
 variable "PUBLIC_SUBNET_ID" {}
+variable "PUBLIC_ZONE_ID" {}

19fc6bf2ca0ddf38aec799e740ddac687462b0ab
Ravi <ravi@gmail.com>
2022-08-07 10:22:23 +0530

    ec2 code

--- private-lb.tf
+++ private-lb.tf
@@ -9,3 +9,21 @@ resource "aws_lb" "alb-private" {
     Environment = "${local.TAG_PREFIX}-private"
   }
 }
+
+resource "aws_lb_listener" "backend" {
+  load_balancer_arn = aws_lb.alb-private.arn
+  port              = 80
+  protocol          = "HTTP"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
